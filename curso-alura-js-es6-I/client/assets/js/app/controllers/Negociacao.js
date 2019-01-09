851bf184ab7afff1e7ac48e64ca30096ae15d4ba
wagner <wagner_gcastro@hotmail.com>
2019-01-09 07:35:28 -0200

    07-Como saber quando o modelo mudou? * Api Reflection e as facetas de this

--- curso-alura-js-es6-I/client/assets/js/app/controllers/Negociacao.js
+++ curso-alura-js-es6-I/client/assets/js/app/controllers/Negociacao.js
@@ -1,12 +1,24 @@
 class Negociacao {
 
+	/**
+	 * [constructor ]
+	 * @return {[empty]} 
+	 */
 	constructor() {
 		let $ 				  	= document.querySelector.bind(document);
 		this._inputData 	  	= $('#data');
 		this._inputQuantidade 	= $('#quantidade');
 		this._inputValor 	  	= $('#valor');
 
-		this._listaNegociacoes 	= new ListaNegociacoes_model();
+		this._listaNegociacoes 	= new ListaNegociacoes_model( 
+			/**
+			 * Obs: Faz a mesma coisa que: this._negociacoesView.loadTemplate(this._listaNegociacoes);
+			 */
+			this,   // this é a class Negociacao
+
+			// esta função será executada, quando os métodos (adiciona,apagar) forem invocados;
+ 			function(model) { this._negociacoesView.loadTemplate(model) }
+ 		);
 		this._negociacoesView   = new Negociacoes_view($('#negociacoes-view')); 
 		this._mensagem 		    = new Mensagem();
 		this._mensagemView  	= new Mensagem_view($('.mensagens')); 
@@ -17,18 +29,25 @@ class Negociacao {
 		
 	}
 
+	/**
+	 * adiciona: 
+	 * @param  {[obj]} event [description]
+	 * @return {[type]}      [description]
+	 */
 	adiciona(event) {
 		event.preventDefault();
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-		this._negociacoesView.loadTemplate(this._listaNegociacoes);
-
-		this._mensagem.texto ='Negociação adicionada com sucesso!'; 
+		this._mensagem.texto = 'Negociação adicionada com sucesso!'; 
 		this._mensagemView.loadTemplate(this._mensagem);
 
 		this._limpaFormulario();
+	}
 
-		// console.log(this._listaNegociacoes.negociacoes);
-		// console.warn(typeof(this._listaNegociacoes.negociacoes));
+
+	apagar() {
+		this._listaNegociacoes.esvazia();
+		this._mensagem.texto ='Lista de Negociação apagada com sucesso!'; 
+		this._mensagemView.loadTemplate(this._mensagem);
 	}
 
 	_criaNegociacao() {
